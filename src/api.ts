e486ed1b0225ed64df444e2140be4137855cdff0
Eluwing <abek0403@gmail.com>
2022-10-12 20:28:37 +0900

    source code add

---
+++ src/api.ts
@@ -0,0 +1,33 @@
+
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+const CHART_BASE_URL = `https://ohlcv-api.nomadcoders.workers.dev`;
+
+export function fetchCoins(){
+    
+    return fetch(`${BASE_URL}/coins`).then((response)=>{
+        return response.json();
+    });
+}
+
+export function fetchCoinInfo(coinId:string){
+
+    return fetch(`${BASE_URL}/coins/${coinId}`).then((response)=>{
+        return response.json();
+    });
+
+}
+
+export function fetchCoinTickers(coinId:string){
+
+    return fetch(`${BASE_URL}/tickers/${coinId}`).then((response)=>{
+        return response.json();
+    });
+
+}
+
+export function fetchCoinHistory(coinId:string){
+    return fetch(`${CHART_BASE_URL}?coinId=${coinId}`).then((response)=>{
+        return response.json();
+    });
+
+}
---
+++ src/styled.d.ts
@@ -0,0 +1,13 @@
+import 'styled-components';
+
+declare module 'styled-components'{
+    export interface DefaultTheme{
+        textColor: string;
+        bgColor: string;
+        accentColor: string;
+        positiveColor: string,
+        negativeColor: string,
+        tabBgColor: string,
+    }
+
+}
